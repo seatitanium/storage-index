590ff37dbf7dc3f3b0c8de66d05a40ad09a15125
Subilan <suyuanliang1995@gmail.com>
2023-07-29 22:15:43 +0800

    init

---
+++ files.js
@@ -0,0 +1,23 @@
+const files = [
+    {
+        displayname: "st7存档",
+        filename: "ST7/world.7z",
+        size: "2.9 GiB",
+        date: "2023/07/29",
+        desc: "st7是2023年开的第一个周目，历时一个多月（6.14-7.29）",
+        compression: {
+            original: 4.9,
+            compressed: 2.9
+        }
+    },
+    {
+        displayname: "st7模组包",
+        filename: "ST7/mods.zip",
+        size: "170 MiB",
+        date: "2023/07/29",
+        compression: {
+            original: 206,
+            compressed: 170
+        }
+    }
+]
