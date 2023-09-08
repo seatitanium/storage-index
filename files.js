2b3ec93713839e462f49e0ae944f65c60fd5d3d1
Subilan <suyuanliang1995@gmail.com>
2023-09-08 11:10:24 +0800

    st9

--- files.js
+++ files.js
@@ -35,10 +35,31 @@ const files = [
         filename: "ST8/world.7z",
         size: "7.8 GiB",
         date: "2023-08-14",
-        desc: "st8使用的是无限整合包，结果只玩了不到半个月...（8.1-8.13）",
+        desc: "st8使用的是无限整合包，虽然说是「无限」但还是只玩了不到半个月就大部分毕业了（8.1-8.13）",
         compression: {
             original: 1136,
             compressed: 784
+        }
+    },
+    {
+        displayname: "st9存档",
+        filename: "ST9/world.zip",
+        size: "572 MiB",
+        date: "2023-09-08",
+        desc: "st9整合包名称为GTQT（机械动力：星辰），但是玩家一致评价其完成度低且bug多（含已完成部分），最终玩不下去了（8.15-9.8）",
+        compression: {
+            original: 822,
+            compressed: 572
+        }
+    },
+    {
+        displayname: "st9模组包",
+        filename: "ST9/mods.zip",
+        size: "419 MiB",
+        date: "2023-09-08",
+        compression: {
+            original: 459,
+            compressed: 419
         }
     }
 ]
